3c33d55134c1c31ed210f41a024d048af19c0845
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-02-15 13:27:03 -0500

    Playing around with node

--- 1.basics/1.let_example.js
+++ 1.basics/1.let_example.js
@@ -6,3 +6,16 @@ it = it ? false : true; // reassign it to be its opposite using the ternary oper
 
 console.log(`It is ${it}!`); // expected output: 'It is false!' - note the string template syntax
 
+
+
+
+alpha = [ 4, 5, 6, ...[ 10, 11, 12 ] ]
+beta = ( x => true )()
+gamma = ( async () => 'hello' )( )
+delta = [...[...[1, 2, 3] ] ].length
+epsilon = ( ( y, x ) => ( x ) ? x : y )( true, false )
+console.log(alpha)
+console.log(beta)
+console.log(gamma)
+console.log(delta)
+console.log(epsilon)
--- 3.functions/4.functions_anonymous_simple.js
+++ 3.functions/4.functions_anonymous_simple.js
@@ -1,6 +1,29 @@
 // define an anonymous function, but assign a variable to refer to it
-const doSomething1 = function () {
-  console.log("doSomething1 is running")
+// const doSomething1 = function () {
+//   console.log("doSomething1 is running")
+// }
+
+// doSomething1() // call the function
+
+doSomething1();
+
+function doSomething1() {
+    console.log('doSomething1');
 }
 
-doSomething1() // call the function
+const doSomething2 = function() {
+    console.log('doSomething2');
+}
+
+const doSomething3 = () => {
+    console.log('doSomething3');
+}
+
+const doSomething4 = () => console.log('doSomething4');
+
+const doSomething5 = new Function("console.log('doSomething5');");
+
+doSomething2();
+doSomething3();
+doSomething4();
+doSomething5();
